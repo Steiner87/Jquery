6f8dd9208929fee9f647e5e98ec1920dc5849177
Igor Pugachov <pugachov.i.87@gmail.com>
2018-06-19 20:14:18 +0300

    add form-validation edition-4

--- form-validation/ajax.js
+++ form-validation/ajax.js
@@ -1,14 +1,26 @@
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+}
+
 function call() {
     var name = $("#name").val();
     var email = $("#email").val();
     var message = $("#message").val();
-    var err = $("#err");
-    if (name == "" || email == "" || message == "") {
+    var err = "";
+    if ((name == "" || name.length <= 3) || (email == "" && !isValidEmailAddress(email)) || (message == "" || message.length < 20)) {
         if (name == "") {
             $("#name").addClass("error");
             $("#name").removeClass("success");
+            err += "Введите имя!<br>";
         }
-        if (name) {
+        if (name.length > 0 && name.length <= 3) {
+            $("#name").addClass("error");
+            $("#name").removeClass("success");
+            err += "Слишком короткое имя!<br>";
+        }
+        if (name.length > 3) {
+
             $("#name").addClass("success");
             $("#name").removeClass("error");
         }
@@ -16,24 +28,36 @@ function call() {
         if (email == "") {
             $("#email").addClass("error");
             $("#email").removeClass("success");
+            err += "Введите почтовый адресс!<br>";
         }
-        if (email) {
+        if (email && !isValidEmailAddress(email)) {
+            $("#email").addClass("error");
+            $("#email").removeClass("success");
+            err += "Неверный формат Email!<br>";
+        }
+        if (isValidEmailAddress(email)) {
             $("#email").addClass("success");
             $("#email").removeClass("error");
         }
         if (message == "") {
             $("#message").addClass("error");
             $("#message").removeClass("success");
+            err += "Введите сообщение!<br>";
         }
-        if (message) {
+        if (message && message.length < 20) {
+            $("#message").addClass("error");
+            $("#message").removeClass("success");
+            err += "Сообщение должно быть больше 20 символов!<br>";
+        }
+        if (message.length > 20) {
             $("#message").addClass("success");
             $("#message").removeClass("error");
         }
-        return err.html("Заполните все поля!");
+        return $("#err").html(err);
     } else {
         $("*").removeClass("error");
         $("*").removeClass("success");
-        err.empty();
+        $("#err").html("");
         var msg = $('#form').serialize();
         $.ajax({
             type: 'POST',
