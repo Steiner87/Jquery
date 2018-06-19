3f0f952b9006f78360b573fa5f569a8f033ff344
Igor Pugachov <pugachov.i.87@gmail.com>
2018-06-19 19:26:51 +0300

    add form-validation edition-3 + style.css

--- form-validation/ajax.js
+++ form-validation/ajax.js
@@ -4,8 +4,35 @@ function call() {
     var message = $("#message").val();
     var err = $("#err");
     if (name == "" || email == "" || message == "") {
+        if (name == "") {
+            $("#name").addClass("error");
+            $("#name").removeClass("success");
+        }
+        if (name) {
+            $("#name").addClass("success");
+            $("#name").removeClass("error");
+        }
+
+        if (email == "") {
+            $("#email").addClass("error");
+            $("#email").removeClass("success");
+        }
+        if (email) {
+            $("#email").addClass("success");
+            $("#email").removeClass("error");
+        }
+        if (message == "") {
+            $("#message").addClass("error");
+            $("#message").removeClass("success");
+        }
+        if (message) {
+            $("#message").addClass("success");
+            $("#message").removeClass("error");
+        }
         return err.html("Заполните все поля!");
     } else {
+        $("*").removeClass("error");
+        $("*").removeClass("success");
         err.empty();
         var msg = $('#form').serialize();
         $.ajax({
