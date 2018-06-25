7af30ec9ced62e559e4de11550c24ad6e7b8dae3
Igor Pugachov <pugachov.i.87@gmail.com>
2018-06-25 17:48:51 +0300

    add to-do-list

---
+++ todo-list/js.js
@@ -0,0 +1,48 @@
+function addTodoItem() {
+    var todoItem = $("#new-todo-item").val();
+    $("#sortable1").append("<li><input type='checkbox'" +
+        " name='todo-item-done'" +
+        " class='todo-item-done'" +
+        " value='" + todoItem + "' /> " +
+        todoItem +
+        " <button class='todo-item-delete'>" +
+        "Delete</button></li>");
+
+    $("#new-todo-item").val("");
+}
+
+function deleteTodoItem(e, item) {
+    e.preventDefault();
+    $(item).parent().fadeOut('slow', function() {
+        $(item).parent().remove();
+    });
+}
+
+
+function completeTodoItem() {
+    $(this).parent().toggleClass("strike");
+}
+
+
+$(function() {
+
+    $("#add-todo-item").on('click', function(e) {
+        e.preventDefault();
+        addTodoItem()
+    });
+
+    //EVENT DELEGATION
+    //#todo-list is the event handler because .todo-item-delete doesn't exist when the document loads, it is generated later by a todo entry
+    //https://learn.jquery.com/events/event-delegation/
+    $("#sortable1").on('click', '.todo-item-delete', function(e) {
+        var item = this;
+        deleteTodoItem(e, item)
+    })
+    $("#sortable2").on('click', '.todo-item-delete', function(e) {
+        var item = this;
+        deleteTodoItem(e, item)
+    })
+
+    $(document).on('click', ".todo-item-done", completeTodoItem)
+
+});
